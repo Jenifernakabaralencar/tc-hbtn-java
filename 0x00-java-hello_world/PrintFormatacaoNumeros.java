f2c449928e00bcd451b7d6e9a5fff4c6f2671784
Jenifer <jnakabar@ciandt.com>
2022-06-13 19:00:14 -0300

    alterando formado do valor

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -6,12 +6,10 @@ public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        Locale localeBR = new Locale("pt","BR");
-        NumberFormat dinheiro = NumberFormat.getCurrencyInstance(localeBR);
-        System.out.println("Valor: " + dinheiro.format(valor));
         DecimalFormat df = new DecimalFormat ("#,##0.00");
+        String valorFormatado = df.format (valor);
+        System.out.println("Valor: R$" + valorFormatado);
         String taxaFormatada = df.format (taxa);
         System.out.println("Taxa: " + taxaFormatada + "%");
     }
 }
-
