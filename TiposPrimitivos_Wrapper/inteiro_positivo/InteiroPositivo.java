e1878869d2bc959f00e194fd856cc9a59f9dd671
Jenifer <jnakabar@ciandt.com>
2022-06-14 18:12:43 -0300

    adicionando programa inteiro Positivo

---
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class InteiroPositivo {
+    public Integer getValor() {
+        return valor;
+    }
+
+    public void setValor(Integer valor) {
+        if (valor > 0) {
+            this.valor = valor;
+        }else {
+            throw new RuntimeException("Valor nao eh um valor inteiro positivo");
+        }
+
+    }
+
+    Integer valor;
+
+    public InteiroPositivo(int x) {
+        this.valor = x;
+    }
+
+    public InteiroPositivo(String y) {
+
+            this.valor = Integer.parseInt(y);
+            if (this.valor < 0){
+                throw new RuntimeException("Valor nao eh um valor inteiro positivo");
+            }
+    }
+
+    public Boolean ehPrimo() {
+        if (this.valor == 1){
+            return false;
+        }
+        for (int x = 2; x < this.valor; x++) {
+            Integer resto = (this.valor % x);
+            if (resto.equals(0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
