aa50fc46f4c09e556ae54d2af826e102180bcb79
Jenifer <jnakabar@ciandt.com>
2022-06-15 14:56:01 -0300

    atualizando arquivos da task 2

--- 0x04/metodos/PersonagemGame.java
+++ 0x04/metodos/PersonagemGame.java
@@ -1,5 +1,3 @@
-package ClassesEObjetos;
-
 public class PersonagemGame {
     private int saudeAtual;
     private String nome;
--- 0x04/metodos/Program.java
+++ 0x04/metodos/Program.java
@@ -1,5 +1,3 @@
-package ClassesEObjetos;
-
 public class Program {
     public static void main(String[] args) {
         PersonagemGame personagem1 = new PersonagemGame();
