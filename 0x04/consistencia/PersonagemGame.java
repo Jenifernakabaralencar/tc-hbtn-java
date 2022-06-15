ffc80f79dbc7f79ce2290fd4d94e79d9dce7ff8b
Jenifer <jnakabar@ciandt.com>
2022-06-15 15:41:54 -0300

    adicionado arquivos da task 4

---
+++ 0x04/consistencia/PersonagemGame.java
@@ -0,0 +1,54 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+
+    private String status;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (this.saudeAtual > 0){
+            this.status = "vivo";
+        }else {
+            this.status = "morto";
+        }
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+       if (nome != null && !nome.isEmpty()){
+            this.nome = nome;
+        };
+    }
+
+    public PersonagemGame(int saudeAtual, String nome) {
+        this.nome = nome;
+        this.setSaudeAtual(saudeAtual);
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        this.saudeAtual -= quantidadeDeDano;
+        if (this.saudeAtual < 0){
+            this.setSaudeAtual(0);
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        this.saudeAtual += quantidadeDeCura;
+        if (this.saudeAtual > 100) {
+            this.setSaudeAtual(100);
+        }else {
+            this.setSaudeAtual(this.saudeAtual);
+        }
+    }
+}
