0e395a2b502278d33d90230e52b249277c9f6665
Jenifer <jnakabar@ciandt.com>
2022-06-21 12:17:12 -0300

    adicionado task 2 de estrutura

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,63 @@
+public class Caractere {
+
+    public static boolean EhMaiusculo(char c){
+        switch (c){
+            case 'A':
+                return true;
+            case 'B':
+                return true;
+            case 'C':
+                return true;
+            case 'D':
+                return true;
+            case 'E':
+                return true;
+            case 'F':
+                return true;
+            case 'G':
+                return true;
+            case 'H':
+                return true;
+            case 'I':
+                return true;
+            case 'J':
+                return true;
+            case 'K':
+                return true;
+            case 'L':
+                return true;
+            case 'M':
+                return true;
+            case 'N':
+                return true;
+            case 'O':
+                return true;
+            case 'P':
+                return true;
+            case 'Q':
+                return true;
+            case 'R':
+                return true;
+            case 'S':
+                return true;
+            case 'T':
+                return true;
+            case 'U':
+                return true;
+            case 'V':
+                return true;
+            case 'W':
+                return true;
+            case 'X':
+                return true;
+            case 'Y':
+                return true;
+            case 'Z':
+                return true;
+            default:
+                return false;
+        }
+
+
+    }
+}
