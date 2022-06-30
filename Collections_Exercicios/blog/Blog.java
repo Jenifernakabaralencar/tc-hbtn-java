280f278a81ed785e8cf9dab1a64a3d59fef308d3
Jenifer <jnakabar@ciandt.com>
2022-06-30 15:12:37 -0300

    adicionando task

--- Collections_Exercicios/blog/Blog.java
+++ Collections_Exercicios/blog/Blog.java
@@ -2,35 +2,35 @@ import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.*;
 
 public class Blog {
-    List<Post> listaDePosts;
+    List<Post> postagens;
 
     public Blog() {
-        listaDePosts = new ArrayList<>();
+        postagens = new ArrayList<>();
     }
 
     public void adicionarPostagem(Post postagem){
-        listaDePosts.add(postagem);
+        postagens.add(postagem);
     }
     public Set<String> obterTodosAutores(){
         Set<String> listaDeAutores = new TreeSet<>();
-        for (int i = 0; i < listaDePosts.size(); i++) {
-            listaDeAutores.add(listaDePosts.get(i).getAutor());
+        for (int i = 0; i < postagens.size(); i++) {
+            listaDeAutores.add(postagens.get(i).getAutor());
         }return listaDeAutores;
     }
     public Map<String, Integer> obterContagemPorCategoria(){
         Map<String, Integer> listaDeCategoria = new TreeMap<>();
-        for (int i = 0; i < listaDePosts.size(); i++){
+        for (int i = 0; i < postagens.size(); i++){
             if (listaDeCategoria.isEmpty()){
-                listaDeCategoria.put(listaDePosts.get(i).getCategoria(),1);
+                listaDeCategoria.put(postagens.get(i).getCategoria(),1);
                 continue;
             }else{
                 for (int j = 0; j < listaDeCategoria.size(); j++) {
-                    if (listaDeCategoria.containsKey(listaDePosts.get(i).getCategoria())){
-                        Integer valor = listaDeCategoria.get(listaDePosts.get(i).getCategoria());
-                        listaDeCategoria.replace(listaDePosts.get(i).getCategoria(), valor+1);
+                    if (listaDeCategoria.containsKey(postagens.get(i).getCategoria())){
+                        Integer valor = listaDeCategoria.get(postagens.get(i).getCategoria());
+                        listaDeCategoria.replace(postagens.get(i).getCategoria(), valor+1);
                         break;
                     }else {
-                        listaDeCategoria.put(listaDePosts.get(i).getCategoria(),1);
+                        listaDeCategoria.put(postagens.get(i).getCategoria(),1);
                         break;
                     }
                 }
