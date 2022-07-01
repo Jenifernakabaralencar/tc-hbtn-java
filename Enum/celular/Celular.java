8d7e48183ff795a8ad9847952d453767bb1b2610
Jenifer <jnakabar@ciandt.com>
2022-07-01 17:29:42 -0300

    ajuste na task 0

--- Enum/celular/Celular.java
+++ Enum/celular/Celular.java
@@ -1,17 +1,17 @@
 import java.util.ArrayList;
 
 public class Celular {
-    private ArrayList<Contato> listaDeContatos;
+    private ArrayList<Contato> contatos;
 
     public Celular() {
-        this.listaDeContatos = new ArrayList<Contato>();
+        this.contatos = new ArrayList<Contato>();
     }
 
     public int obterPosicaoContato(String nomeDoContato) {
         //i é a posição da lista
         //listaDeContatos.size é o tamanho da lista
-        for (int i = 0; i < listaDeContatos.size(); i++) {
-            if (listaDeContatos.get(i).getNome().equals(nomeDoContato)) {
+        for (int i = 0; i < contatos.size(); i++) {
+            if (contatos.get(i).getNome().equals(nomeDoContato)) {
                 return i;
             }
         }
@@ -20,7 +20,7 @@ public class Celular {
 
     public void adicionarContato(Contato novoContato) {
         if (obterPosicaoContato(novoContato.getNome()) == -1) {
-            listaDeContatos.add(novoContato);
+            contatos.add(novoContato);
         } else {
             throw new IllegalArgumentException("Nao foi possivel adicionar contato. Contato jah existente com esse nome");
         }
@@ -36,8 +36,8 @@ public class Celular {
                 //novo ja existe entao lancar erro
                 throw new IllegalArgumentException("Nao foi possivel modificar contato. Contato jah existente com esse nome");
             }//novo contato nao existe atualizar o antigo
-            int i = listaDeContatos.indexOf(contatoAntigo);
-            listaDeContatos.set(i, novoContato);
+            int i = contatos.indexOf(contatoAntigo);
+            contatos.set(i, novoContato);
         }
     }
 
@@ -45,14 +45,14 @@ public class Celular {
         if (obterPosicaoContato(contato.getNome()) == -1) {
             throw new IllegalArgumentException("Nao foi possivel remover contato. Contato nao existe");
         }
-        listaDeContatos.remove(contato);
+        contatos.remove(contato);
     }
 
     public void listarContatos() {
-        for (int i = 0; i < listaDeContatos.size(); i++) {
-            String nomes = listaDeContatos.get(i).getNome();
-            String numeroDeTelefone = listaDeContatos.get(i).getNumeroDeTelefone();
-            TipoNumero tipo = listaDeContatos.get(i).getTipo();
+        for (int i = 0; i < contatos.size(); i++) {
+            String nomes = contatos.get(i).getNome();
+            String numeroDeTelefone = contatos.get(i).getNumeroDeTelefone();
+            TipoNumero tipo = contatos.get(i).getTipo();
             System.out.printf("%s -> %s (%s)\n", nomes, numeroDeTelefone, tipo);
         }
     }
