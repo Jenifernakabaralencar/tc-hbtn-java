ee48f90baca01bb337c47c74e23e290976ab40d5
Jenifer <jnakabar@ciandt.com>
2022-07-07 15:18:35 -0300

    adicionando task 0

---
+++ Lambda/markup/Produto.java
@@ -0,0 +1,32 @@
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private double percentualMarkup = 10;
+
+    Supplier<Double> precoComMarkup = () -> preco + (preco * percentualMarkup);
+    Consumer<Double> atualizarMarkup = (Double percentualMarkup) -> setPercentualMarkup(percentualMarkup/100);
+
+    public Produto(double preco, String nome) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public double getPercentualMarkup() {
+        return percentualMarkup;
+    }
+
+    public void setPercentualMarkup(double percentualMarkup) {
+        this.percentualMarkup = percentualMarkup;
+    }
+}
